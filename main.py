699284f6c605a802589d50e3929832f63a8d5ac4
oscarschenk <79904169+oscarschenk@users.noreply.github.com>
2022-05-13 11:32:12 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+
+gullit = 'Ruud Gullit'
+basten = 'Marco van Basten'
+
+goal_0 = 32
+goal_1 = 54
+
+scorers = f'{gullit} {goal_0}, {basten} {goal_1}'
+
+report = f'{gullit} scored in the {goal_0}nd minute {basten} scored in the {goal_1}th minute'
+
+print(scorers)
+
+player = 'Vasyl Rats'
+
+first_name = player[0:5]
+
+last_name = player[6:12]
+
+name_short = f'{first_name[0:1]}. {last_name}'
+
+chant = f'{first_name}! {first_name}! {first_name}! {first_name}! {first_name}!'
+
+print(report)
